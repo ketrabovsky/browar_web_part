b16eaf81241f7c54aabf15fb0d98148ff29282b1
Szymon Anczak <szymon0antczak@gmail.com>
2019-05-08 17:24:49 +0200

    Porządeczki

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // $('button#heater').click(function(){
+    //     $.ajax({
+    //         url: "/_get_data/",
+    //         type: "POST",
+    //         success: function(resp){
+    //             console.log(resp);
+    //             $('button#heater').toggleClass('active');
+    //         }
+    //     });
+    // });
+
+    $('button#heater').click(function() {
+       $(this).toggleClass('active');
+    })
+});
